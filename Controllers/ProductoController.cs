fa6027e975d1c03bea230618ba2dd80e968a31e8
DoAle34411 <domenica.arcos.lazo@udla.edu.ec>
2023-12-09 17:06:40 -0500

    WIP sesion de vistas secundarias a principal

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,6 +1,7 @@
 ﻿using CRUD_MVC.Models;
 using CRUD_MVC.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.ConstrainedExecution;
 using System.Text.Json;
 
 namespace CRUD_MVC.Controllers
@@ -16,6 +17,7 @@ namespace CRUD_MVC.Controllers
 
 
         // GET: ProductoController
+        [HttpPost]
         public async Task<IActionResult> Index()
         {
             if (HttpContext.Request.Method == "POST")
@@ -29,50 +31,69 @@ namespace CRUD_MVC.Controllers
             return View(productos);
         }
 
-        public async Task<IActionResult> Details(int IdProducto)
+        public async Task<IActionResult> Index(int IdUsuario)
         {
+            Console.WriteLine(IdUsuario);
+            var user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
+
+            // Rest of your code
+            var productos = await _APIServices.GetProducts();
+            return View(productos);
+        }
+
+
+        public async Task<IActionResult> Details(int IdProducto, int IdUsuario)
+        {
+            User user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
             var producto = await _APIServices.GetProduct(IdProducto);
             if (producto != null) return View(producto);
             return RedirectToAction("Index");
         }
 
-        // GET: ProductoController/Create
-        public IActionResult Create()
+        public IActionResult Create(int IdUsuario)
         {
             return View();
         }
 
         [HttpPost]
-        /*public IActionResult Create(Producto producto)
-        {
-            int id = Utils.ListaProducto.Count() + 1;
-            producto.IdProducto = id;
-            Utils.ListaProducto.Add(producto);
-            return RedirectToAction("Index");
-        }*/
-
         public async Task<IActionResult> Create(Producto producto)
         {
             await _APIServices.POSTProducto(producto);
             return RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> Edit(int IdProducto)
+        public async Task<IActionResult> Edit(int IdProducto, int IdUsuario)
         {
+            User user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
             var producto = await _APIServices.GetProduct(IdProducto);
             if (producto != null) return View(producto);
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(Producto producto)
+        public async Task<IActionResult> Edit(int IdProducto, string Nombre, string Descripcion, int Cantidad, string Autor, string Genero, int IdUsuario)
         {
+            Producto producto = new Producto();
+            Console.WriteLine(IdProducto+Nombre);
+            producto.IdProducto = IdProducto;
+            producto.Nombre = Nombre;
+            producto.Descripcion = Descripcion;
+            producto.Cantidad=Cantidad;
+            producto.Autor = Autor;
+            producto.Genero = Genero;
+            Console.WriteLine(IdUsuario);
             await _APIServices.PUTProducto(producto.IdProducto, producto);
-            return RedirectToAction("Index");
+            Console.WriteLine(1122);
+            return RedirectToAction("Index", IdUsuario);
         }
 
-        public async Task<IActionResult> Delete(int IdProducto)
+        public async Task<IActionResult> Delete(int IdProducto, int IdUsuario)
         {
+            User user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
             Console.WriteLine($"El Id enviado fue: {IdProducto}");
             await _APIServices.DeleteProducto(IdProducto);
 
