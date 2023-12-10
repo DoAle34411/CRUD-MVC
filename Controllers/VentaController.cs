00a3adac562af3ff27d71e2b5b9f3018244c055c
DoAle34411 <domenica.arcos.lazo@udla.edu.ec>
2023-12-09 22:28:18 -0500

    Servicio Rentas Implementado

---
+++ Controllers/VentaController.cs
@@ -0,0 +1,88 @@
+﻿using CRUD_MVC.Models;
+using CRUD_MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CRUD_MVC.Controllers
+{
+    public class VentaController : Controller
+    {
+        private readonly IAPIServices _APIServices;
+
+        public VentaController(IAPIServices servicios)
+        {
+            _APIServices = servicios;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Request.Method == "POST")
+            {
+                var userJson = HttpContext.Request.Form["user"];
+                var user = JsonSerializer.Deserialize<User>(userJson, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                ViewBag.User = user;
+                Console.WriteLine(user.CodigoAcceso);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Index(int IdUsuario)
+        {
+            var user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
+            return View();
+        }
+        public async Task<IActionResult> Devolver(int IdUsuario)
+        {
+            User user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Devolver(int IdUsuario, int IdLibro, int IdUsuarioActivo)
+        {
+            Console.WriteLine("hola2");
+            Console.WriteLine(IdUsuario);
+            Console.WriteLine(IdLibro);
+            Console.WriteLine(IdUsuarioActivo);
+            User user = await _APIServices.GetUser(IdUsuario);
+            Producto producto = await _APIServices.GetProduct(IdLibro);
+            if (user.HaRetirado) 
+            {
+                user.HaRetirado = false;
+                user.IdLibroRetirado = 0;
+                await _APIServices.PutUser(user.IdUsuario, user);
+                producto.Cantidad++;
+                await _APIServices.PUTProducto(producto.IdProducto, producto);
+            }
+            return RedirectToAction("Index", new { IdUsuario = IdUsuarioActivo });
+        }
+        public async Task<IActionResult> Retirar(int IdUsuario)
+        {
+            User user = await _APIServices.GetUser(IdUsuario);
+            ViewBag.User = user;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Retirar(int IdUsuario, int IdLibro, int IdUsuarioActivo)
+        {
+            Console.WriteLine("hola");
+            Console.WriteLine(IdUsuario);
+            Console.WriteLine(IdLibro);
+            Console.WriteLine(IdUsuarioActivo);
+            User user = await _APIServices.GetUser(IdUsuario);
+            Producto producto = await _APIServices.GetProduct(IdLibro);
+            if (!user.HaRetirado)
+            {
+                user.HaRetirado = true;
+                user.IdLibroRetirado = IdLibro;
+                await _APIServices.PutUser(user.IdUsuario, user);
+                producto.Cantidad--;
+                await _APIServices.PUTProducto(producto.IdProducto, producto);
+            }
+            return RedirectToAction("Index", new { IdUsuario = IdUsuarioActivo });
+        }
+    }
+}
---
+++ Models/Temporal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_MVC.Models
+{
+    public class Temporal
+    {
+        [Required]
+        public int IdUsuario { get; set; }
+        [Required]
+        public int IdLibro { get; set; }
+        [Required]
+        public int IdUsuarioActivo { get; set; }
+
+    }
+}
